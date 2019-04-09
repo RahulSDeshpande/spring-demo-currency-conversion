7abb75d8bfb4d79b42b2e2e583a860887b442e1c
RD <rahul.deshpande92@gmail.com>
2019-04-10 01:29:16 +0530

    - Integrated Ribbon in the project

--- src/main/java/com/rahulografy/springdemo/currencyconversion/CurrencyExchangeControllerProxy.java
+++ src/main/java/com/rahulografy/springdemo/currencyconversion/CurrencyExchangeControllerProxy.java
@@ -1,12 +1,15 @@
 package com.rahulografy.springdemo.currencyconversion;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.rahulografy.springdemo.currencyconversion.bean.ConversionValue;
 
-@FeignClient(name = "service-exchange-conversion", url = "localhost:8000")
+// @FeignClient(name = "service-exchange-conversion", url = "localhost:8000")
+@FeignClient(name = "service-exchange-conversion")
+@RibbonClient(name = "service-exchange-conversion")
 public interface CurrencyExchangeControllerProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
